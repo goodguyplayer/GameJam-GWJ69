d3a4b8395dbc56ef27c0aa9662a9e3c08148135a
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-10 20:36:34 -0300

    Started making the menu

---
+++ GameFiles/Menu/MainMenu/main_menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var title = $MarginContainer/HBoxContainer/VBoxContainer2/Title
+@onready var subtitle = $MarginContainer/HBoxContainer/VBoxContainer2/Subtitle
+@onready var subsubtitle = $MarginContainer/HBoxContainer/VBoxContainer2/Subsubtitle
+
+@onready var start = $MarginContainer/HBoxContainer/VBoxContainer/Start
+@onready var options = $MarginContainer/HBoxContainer/VBoxContainer/Options
+@onready var credits = $MarginContainer/HBoxContainer/VBoxContainer/Credits
+@onready var exit = $MarginContainer/HBoxContainer/VBoxContainer/Exit
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	pass # Replace with function body.
