19f4bfcd45abcefaeda50fba0e9b67af296e614b
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-11 14:22:34 -0300

    Added Dash movement

--- GameFiles/Mechanics/Player/player.gd
+++ GameFiles/Mechanics/Player/player.gd
@@ -1,7 +1,11 @@
 extends RigidBody2D
 
 
-@export var push_force = 1
+@export var push_force = 1000
+@export var dash_force = 50
+@export var dash_delay : float = 3.0
+
+@onready var dash_timer : Timer = $DashTimer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +21,15 @@ func _process(delta):
 func _physics_process(delta):
 	var input_direction = Input.get_axis("input_left", "input_right")
 	var force_to_push = Vector2(input_direction * push_force, 0)
-	apply_force(force_to_push)
 	
+	if dash_timer.is_stopped():
+		if Input.is_action_pressed("input_dash"):
+			force_to_push *= dash_force
+			dash_timer.start(dash_delay)
+			
+	apply_force(force_to_push)
+
+
+
+func _on_dash_timer_timeout():
+	print("Timer stopped")
