87cb8f7864abc0a364509efa8678f1fa45a806c7
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-11 20:05:07 -0300

    Triggerbox update

--- GameFiles/Mechanics/HitHurtTriggerBox/triggerbox.gd
+++ GameFiles/Mechanics/HitHurtTriggerBox/triggerbox.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 
+@export var action : Action
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_entered(area):
+	if action:
+		action.execute()
