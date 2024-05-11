2deb17e2792f5a596e25418c4a08be9894b35f04
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-11 19:54:34 -0300

    Init gameover

---
+++ GameFiles/UI/GameOver/game_over.gd
@@ -0,0 +1,15 @@
+extends Control
+
+const MENU_PATH : String = "res://Menu/MainMenu/main_menu.tscn"
+
+
+func game_over() -> void:
+	visible = true
+
+
+func _on_try_again_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_return_menu_pressed():
+	get_tree().change_scene_to_file(MENU_PATH)
