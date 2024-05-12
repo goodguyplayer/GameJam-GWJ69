e28d09e0ec92423619af06e18298763e15145582
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-12 16:18:00 -0300

    Added shader to scrolling scene
    I need to learn about shaders

--- GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
+++ GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
@@ -1,5 +1,5 @@
-extends ParallaxBackground
+extends Node2D
 
 
-func _process(delta):
-	scroll_base_offset -= Vector2(0, 40) * delta
+#func _process(delta):
+	#scroll_base_offset -= Vector2(0, 40) * delta
