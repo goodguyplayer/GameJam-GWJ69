f8aa96199e8c20b31a823f2acfb0cac3d4b93b4c
goodguyplayer <nathanbritodasilva@gmail.com>
2024-05-13 14:21:30 -0300

    Fixed main game w shader

--- GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
+++ GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 @onready var shader_material = preload("res://Mechanics/ScrollingScene/background_shader.tres")
+@onready var sprite_2d : Sprite2D = $Sprite2D
 
 
 func _ready():
 	if OptionsAutoload.graphics_simple:
-		self.material = shader_material
+		sprite_2d.material = shader_material
 
