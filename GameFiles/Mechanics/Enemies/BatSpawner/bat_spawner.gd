1f791e675ad94ff268f324572305471d4b68923c
goodguyplayer <nathanbritodasilva@gmail.com>
2024-05-13 16:01:28 -0300

    HazardManager update - Spawn bats
    Issue is to make it fun now, cuz it kinda looks boring atm

--- GameFiles/Mechanics/Enemies/BatSpawner/bat_spawner.gd
+++ GameFiles/Mechanics/Enemies/BatSpawner/bat_spawner.gd
@@ -1,3 +1,4 @@
+class_name BatSpawner
 extends Node2D
 
 
--- GameFiles/Mechanics/HazardManager/hazard_manager.gd
+++ GameFiles/Mechanics/HazardManager/hazard_manager.gd
@@ -1,11 +1,31 @@
 extends Node2D
 
+@export var bat_spawner_array : Array[BatSpawner]
+@export var time_delay_min : float = 3.0
+@export var time_delay_max : float = 4.0
+@export var increment : float = 0.5
+@export var bats_per_spawn : int = 3
 
-# Called when the node enters the scene tree for the first time.
+@onready var timer : Timer = $Timer
+
+
 func _ready():
-	pass # Replace with function body.
+	timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _spawn_bats() -> void:
+	var unique_spawners = []
+	var to_spawn = bats_per_spawn
+	var bat_spawner : BatSpawner
+	while to_spawn > 0:
+		
+		bat_spawner = bat_spawner_array[RandomizerAutoload.rng.randi_range(0, len(bat_spawner_array) - 1)]
+		if bat_spawner not in unique_spawners:
+			bat_spawner.spawn_bat()
+			unique_spawners.append(bat_spawner)
+			to_spawn -= 1
+	timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
+		
+
+func _on_timer_timeout():
+	_spawn_bats()
