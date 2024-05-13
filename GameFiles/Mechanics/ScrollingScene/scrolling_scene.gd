40f0089fe8e4655196f0a267ea34df778c22be3f
goodguyplayer <nathanbritodasilva@gmail.com>
2024-05-13 12:15:18 -0300

    Options Autoload update
    Updated code to now load inputs and write them;
    Also updated the code to have a "graphics" key section

--- GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
+++ GameFiles/Mechanics/ScrollingScene/scrolling_scene.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var shader_material = preload("res://Mechanics/ScrollingScene/background_shader.tres")
 
+
 func _ready():
 	if !OptionsAutoload.graphics_simple:
 		self.material = shader_material
-#func _process(delta):
-	#scroll_base_offset -= Vector2(0, 40) * delta
+
--- GameFiles/Scripts/Autoloads/options.gd
+++ GameFiles/Scripts/Autoloads/options.gd
@@ -10,18 +10,23 @@ var save_path = SAVE_DIR + "options_config" + FILE_FORMAT
 @export var audio_volume : float = 1
 @export var audio_enabled : bool = true
 @export var graphics_simple : bool = false
+@export var input_array : Array[String] = [
+	"input_left",
+	"input_right",
+	"input_dash",
+	"input_pause",
+]
 
 var music_volume_default : float = 1
 var music_enabled_default : bool = true
 var audio_volume_default : float = 1
 var audio_enabled_default : bool = true
+var graphics_simple_default : bool = false
 @export var default_config : Dictionary = {
-		#"ui_up" : 87,
-		#"ui_left" : 65,
-		#"ui_right" : 68,
-		#"ui_down" : 83,
-		#"ui_pause" : 80,
-		#"ui_textbutton" : 16777221,
+		"input_left" : KEY_A,
+		"input_right" : KEY_D,
+		"input_dash" : KEY_SHIFT,
+		"input_pause" : KEY_ESCAPE,
 	}
 	
 
@@ -33,11 +38,20 @@ func _ready() -> void:
 	load_file()
 		
 
-func save_options(options_dict : Dictionary) -> void:
+func save_options() -> void:
 	var config = ConfigFile.new()
 	var err = config.load(save_path)
 	if err == OK:
-		pass
+		config.set_value("audio_music", "music_volume", music_volume)
+		config.set_value("audio_music", "music_enabled", music_enabled)
+		config.set_value("audio_music", "audio_volume", audio_volume)
+		config.set_value("audio_music", "audio_enabled", audio_enabled)
+		
+		config.set_value("graphics", "graphics_simple", graphics_simple)
+		for i in input_array:
+			save_input(i, config)
+		# Save inputs
+		config.save(save_path)
 		
 
 func load_file() -> void:
@@ -48,6 +62,11 @@ func load_file() -> void:
 		music_enabled = config.get_value("audio_music", "music_enabled")
 		audio_volume = config.get_value("audio_music", "audio_volume")
 		audio_enabled = config.get_value("audio_music", "audio_enabled")
+		
+		graphics_simple = config.get_value("graphics", "graphics_simple")
+		
+		for i in input_array:
+			load_input(i, config)
 	
 
 
@@ -57,6 +76,23 @@ func reset_default() -> void:
 	config.set_value("audio_music", "music_enabled", music_enabled_default)
 	config.set_value("audio_music", "audio_volume", audio_volume_default)
 	config.set_value("audio_music", "audio_enabled", audio_enabled_default)
+	
+	config.set_value("graphics", "graphics_simple", graphics_simple_default)
 	# Save inputs
+	for i in input_array:
+		config.set_value("inputs", i, default_config[i])
+	
 	config.save(save_path)
 	
+
+func save_input(action_name, config_link : ConfigFile) -> void:
+	var save_input_event = InputMap.action_get_events(action_name)[0]
+	config_link.set_value("inputs", action_name, save_input_event.keycode)
+	
+	
+
+func load_input(action_name, config_link : ConfigFile) -> void:
+	var load_input_event = InputEventKey.new()
+	load_input_event.keycode = config_link.get_value("inputs", action_name)
+	InputMap.action_erase_events(action_name)
+	InputMap.action_add_event(action_name, load_input_event)
