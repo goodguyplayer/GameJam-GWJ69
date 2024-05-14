106a9896fef75953cf34aa46945dfbcf5f999faa
goodguyplayer <nathanbritodasilva@gmail.com>
2024-05-14 11:07:35 -0300

    ScoreCounter init
    Not a fan of saving code in a non autoload node, but could be a new experience.

---
+++ GameFiles/Mechanics/ScoreCounter/score_counter.gd
@@ -0,0 +1,40 @@
+class_name ScoreCounter
+extends Node
+
+const SAVE_DIR = "user://"
+const FILE_FORMAT = ".cfg"
+const FILE_NAME = "score_config"
+
+@export var highscore : int = 0
+@export var current_score : int = 0
+
+var config : ConfigFile
+var save_path = SAVE_DIR + FILE_NAME + FILE_FORMAT
+
+
+func _ready():
+	config = ConfigFile.new()
+	var err = config.load(save_path)
+	if err != OK:
+		save_score(0, 0)
+	
+	highscore = config.get_value("score", "highscore")
+	current_score = config.get_value("score", "current_score")
+	SignalsAutoload.player_fell.connect(on_quit)
+
+
+func _process(delta):
+	current_score += 1
+	if current_score >= highscore:
+		highscore = current_score
+	SignalsAutoload.score_changed.emit(current_score, highscore)
+		
+
+func save_score(high, current) -> void:
+	config.set_value("score", "highscore", high)
+	config.set_value("score", "current_score", current)
+	config.save(save_path)
+
+
+func on_quit() -> void:
+	save_score(highscore, current_score)
--- GameFiles/Scripts/Autoloads/signals.gd
+++ GameFiles/Scripts/Autoloads/signals.gd
@@ -1,5 +1,6 @@
 extends Node
 
 signal player_health_changed(old, new)
+signal score_changed(current, highscore)
 signal player_died()
 signal player_fell()
