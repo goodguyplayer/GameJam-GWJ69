0d3bdce639196d1af0e7ab2736bf4c7e5ac699c7
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-13 23:04:33 -0300

    Added voidHoleSpawner to hazard_manager

--- GameFiles/Mechanics/HazardManager/hazard_manager.gd
+++ GameFiles/Mechanics/HazardManager/hazard_manager.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 @export var bat_spawner_array : Array[BatSpawner]
+@export var void_hole_array : Array[VoidHoleSpawner]
 @export var time_delay_min : float = 3.0
 @export var time_delay_max : float = 4.0
 @export var increment : float = 0.5
 @export var bats_per_spawn : int = 3
+@export var voids_per_spawn : int = 2
 
 @onready var timer : Timer = $Timer
+@onready var void_timer : Timer = $VoidTimer
 
 
 func _ready():
 	timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
+	void_timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
 
 
 func _spawn_bats() -> void:
@@ -25,7 +29,25 @@ func _spawn_bats() -> void:
 			unique_spawners.append(bat_spawner)
 			to_spawn -= 1
 	timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
+	
+
+func _spawn_void_holes() -> void:
+	var unique_spawners = []
+	var to_spawn = voids_per_spawn
+	var void_spawner : VoidHoleSpawner
+	while to_spawn > 0:
+		
+		void_spawner = void_hole_array[RandomizerAutoload.rng.randi_range(0, len(void_hole_array) - 1)]
+		if void_spawner not in unique_spawners:
+			void_spawner.start_spawning_void()
+			unique_spawners.append(void_spawner)
+			to_spawn -= 1
+	void_timer.start(RandomizerAutoload.rng.randf_range(time_delay_min, time_delay_max))
 		
 
 func _on_timer_timeout():
 	_spawn_bats()
+
+
+func _on_void_timer_timeout():
+	_spawn_void_holes()
