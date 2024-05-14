bf08413de4dd761475e3625b9f2fe226f049f602
goodguyplayer <nathanbritodasilva@gmail.com>
2024-05-14 15:08:34 -0300

    CollectableItemSpawner init

---
+++ GameFiles/Mechanics/Pickups/CollectableItemSpawner/collectable_item_spawner.gd
@@ -0,0 +1,18 @@
+class_name CollectableItemSpawner
+extends Node2D
+
+@export_enum("SCORE_INCREASE", "SCORE_BOOST") var spawner
+
+@onready var marker_2d : Marker2D = $Marker2D
+
+var path_array : Array = [
+	"res://Mechanics/Pickups/CollectableItem/score_increase_item.tscn"
+] 
+
+
+func spawn_colletable() -> void:
+	print("SPAWNING")
+	var to_spawn : CollectableItem = load(path_array[spawner]).instantiate()
+	to_spawn.direction = (self.global_position - marker_2d.global_position).normalized()
+	to_spawn.position = marker_2d.global_position
+	get_tree().current_scene.add_child(to_spawn)
