e304db28d484466b5c48296508e4769db485dcf8
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2024-05-18 10:11:10 -0300

    Bug fix attempt

--- GameFiles/Mechanics/Player/player_rope.gd
+++ GameFiles/Mechanics/Player/player_rope.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 @onready var pin_joint_2d : PinJoint2D = $PinJoint2D
 
+var player_died : bool = false
+
 
 func _ready():
 	SignalsAutoload.player_died.connect(_on_player_player_died)
 
 
 func _on_player_player_died():
-	pin_joint_2d.queue_free()
+	if !player_died:
+		pin_joint_2d.queue_free()
+		player_died = true
 	pass
